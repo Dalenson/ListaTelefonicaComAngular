b7a868cff4da5b4c8445800338b1a54b3b4a138f
Dalençon Cardoso Colares <108817919+Dalenson@users.noreply.github.com>
2022-10-19 18:45:53 -0300

    Ajuste

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(process.env.PORT || 5000);
