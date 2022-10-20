25bfbcf7c987c52b9adcb5a678fb9098e995347a
Dalenson <dal.colares@hotmail.com>
2022-10-19 21:21:21 -0300

    Ajuste

---
+++ bower.js
@@ -0,0 +1,20 @@
+{
+    "name": "gameJq",
+    "description": "",
+    "main": "server.js",
+    "authors": [
+      "dalencon.colares <dal.colares@hotmail.com>"
+    ],
+    "license": "ISC",
+    "homepage": "",
+    "ignore": [
+      "**/.*",
+      "test",
+      "tests"
+    ],
+    "dependencies": {
+      "angular-route": "^1.8.3",
+      "angular": "^1.8.3",
+      "restangular": "^1.6.1"
+    }
+  }
